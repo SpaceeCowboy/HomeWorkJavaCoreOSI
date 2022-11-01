b632f302e3fa135ae09005a01a1c30e5d44655d0
Alexey Shapilov <lshapilov@bk.ru>
2022-11-01 23:11:27 +0300

    homework

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Server start");
+        int port = 8089;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ) {
+                    System.out.println("Port: " + clientSocket.getPort());
+                    final String name = in.readLine();
+                    out.println("Hi " + name + " your port is " + clientSocket.getPort());
+                }
+
+            }
+        }
+    }
+}
